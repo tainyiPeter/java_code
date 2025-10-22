7956b50fde1f63428f27442e0a7099a116c1d2f7
deep\peter <pybmfc@163.com>
2025-10-22 21:29:49 +0800

    暂存文件

---
+++ my-webapp/src/main/java/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello, World!</h1>");
+            out.println("<p>当前时间: " + new java.util.Date() + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+}
---
+++ my-webapp/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>我的Web应用</title>
+</head>
+<body>
+<h1>欢迎来到我的Web应用</h1>
+<p>这是一个使用 Maven 构建的 Java Web 应用</p>
+<ul>
+    <li><a href="hello">访问 Hello Servlet</a></li>
+</ul>
+</body>
+</html>
