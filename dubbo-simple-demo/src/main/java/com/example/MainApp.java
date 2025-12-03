cf8b028ad8874e9bfee63334e4fd4bf64c1473a4
panyuanbo <279261136@qq.com>
2025-12-03 13:25:21 +0800

    dubbo 20880 ，telnet成功

--- dubbo-simple-demo/src/main/java/com/example/MainApp.java
+++ dubbo-simple-demo/src/main/java/com/example/MainApp.java
@@ -1,6 +1,6 @@
 package com.example;
 
-// import com.alibaba.dubbo.container.Main;
+import com.alibaba.dubbo.container.Main;
 
 /**
  * 最简单的Dubbo容器启动示例
@@ -12,17 +12,17 @@ public class MainApp {
         System.out.println("服务接口: com.example.service.HelloService");
         System.out.println("日志输出到控制台...");
 
-        // 方式1：设置系统属性指定配置文件
+        // 禁用XML验证
+        System.setProperty("spring.xml.validation.mode", "IGNORE");
+
+        // 设置系统属性指定配置文件
         System.setProperty("dubbo.spring.config",
                 "classpath:META-INF/spring/dubbo-provider.xml");
 
-        // 方式2：使用dubbo.properties文件（推荐）
-        // 在resources目录下创建dubbo.properties文件
+        // 设置要启动的容器
+        String[] newArgs = new String[]{"spring"};
 
         // 启动Dubbo容器
-        // 参数为空时，默认加载classpath*:META-INF/spring/*.xml
-        //Main.main(args);
-
-        com.alibaba.dubbo.container.Main.main(args);
+        Main.main(newArgs);
     }
 }
