c3fb3dc288b6aa0417296c86cd6c01695836311a
panyuanbo <279261136@qq.com>
2025-12-03 14:25:39 +0800

    恢复dubbo-provider配置，不使用纯的配置

--- dubbo-simple-demo/start.bat
+++ dubbo-simple-demo/start.bat
@@ -1,9 +1,7 @@
 @echo off 
 
-echo === 启动 Dubbo 服务 ===
+echo "=== 启动 Dubbo 服务 ==="
 java -cp "target\classes;target\lib\*" ^
-     -Ddubbo.spring.config=classpath*:META-INF/spring/*.xml ^
-     com.alibaba.dubbo.container.Main ^
-     spring
-	 
+	-Ddubbo.spring.config=classpath*:META-INF/spring/*.xml ^
+     com.alibaba.dubbo.container.Main 
 pause
