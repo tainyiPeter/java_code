0bf2bf58c529973107411d5d611e695ccb85c788
panyuanbo <279261136@qq.com>
2026-01-07 09:08:14 +0800

    add vtk demo

---
+++ vtk_test/demo1/demo1.cpp
@@ -0,0 +1,47 @@
+﻿#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+#include <vtkConeSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+
+int main() {
+	// 创建圆锥源
+	vtkSmartPointer<vtkConeSource> cone = vtkSmartPointer<vtkConeSource>::New();
+	cone->SetHeight(3.0);
+	cone->SetRadius(1.0);
+	cone->SetResolution(10);
+
+	// 创建映射器
+	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(cone->GetOutputPort());
+
+	// 创建演员
+	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	// 创建渲染器
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+	renderer->SetBackground(0.1, 0.2, 0.4);
+
+	// 创建渲染窗口
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+	renderWindow->SetSize(800, 600);
+
+	// 创建交互器
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+
+	// 开始渲染
+	renderWindow->Render();
+	interactor->Start();
+
+	return 0;
+}
