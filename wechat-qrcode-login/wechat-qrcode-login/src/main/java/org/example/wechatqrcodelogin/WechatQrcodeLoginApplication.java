fa79e719217b56941e9587fe4b8ac50bdb7e9d7f
panyuanbo <279261136@qq.com>
2026-02-16 13:32:59 +0800

    原始工程

---
+++ wechat-qrcode-login/wechat-qrcode-login/src/main/java/org/example/wechatqrcodelogin/WechatQrcodeLoginApplication.java
@@ -0,0 +1,13 @@
+package org.example.wechatqrcodelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatQrcodeLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatQrcodeLoginApplication.class, args);
+    }
+
+}
---
+++ wechat-qrcode-login/wechat-qrcode-login/src/test/java/org/example/wechatqrcodelogin/WechatQrcodeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wechatqrcodelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatQrcodeLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
