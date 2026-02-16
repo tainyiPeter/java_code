dc1919b24af0d09c874e2312648976ad752c469d
panyuanbo <279261136@qq.com>
2026-02-16 15:17:11 +0800

    启用  @Data 注解

--- wechat-qrcode-login/src/main/java/com/example/wechat/model/ScanCallbackRequest.java
+++ wechat-qrcode-login/src/main/java/com/example/wechat/model/ScanCallbackRequest.java
@@ -1,46 +1,9 @@
 package com.example.wechat.model;
 
-// 移除 Lombok 的 import
-// import lombok.Data;
+import lombok.Data;
 
-// @Data 注解也移除
+@Data
 public class ScanCallbackRequest {
     private String loginToken;  // 二维码中的登录令牌
     private String code;        // 小程序wx.login获取的code
-
-    // 无参构造方法
-    public ScanCallbackRequest() {
-    }
-
-    // 有参构造方法（可选）
-    public ScanCallbackRequest(String loginToken, String code) {
-        this.loginToken = loginToken;
-        this.code = code;
-    }
-
-    // Getter 和 Setter 方法
-    public String getLoginToken() {
-        return loginToken;
-    }
-
-    public void setLoginToken(String loginToken) {
-        this.loginToken = loginToken;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    // toString 方法（方便调试）
-    @Override
-    public String toString() {
-        return "ScanCallbackRequest{" +
-                "loginToken='" + loginToken + '\'' +
-                ", code='" + code + '\'' +
-                '}';
-    }
 }
